a5811313bc0490a4bfcc9a5c167df7965796410e
hector59a2b9297 <hector.02220x@gmail.com>
2025-04-22 23:30:08 -0300

    Feat: Database context created

---
+++ UsersAuthenticationApi/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UsersAuthenticationApi.Models;
+
+namespace UsersAuthenticationApi.Data
+{
+  public class AppDbContext : DbContext
+  {
+    public DbSet<UserModel> Users { get; set; }
+
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) {}
+  }
+}
